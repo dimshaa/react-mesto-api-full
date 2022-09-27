6d1320c09b110bf6a95a440992fcca1cd5531148
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-09-27 18:36:20 +0300

    delete unused import

--- frontend/src/components/EditAvatarPopup.js
+++ frontend/src/components/EditAvatarPopup.js
@@ -1,5 +1,4 @@
-import { useContext, useEffect, useRef } from "react";
-import CurrentUserContext from "../contexts/CurrentUserContext.js";
+import { useEffect, useRef } from "react";
 import PopupWithForm from "./PopupWithForm.js";
 
 function EditAvatarPopup(props) {
